b2455e6e57e4b0e997bdc9a55f221a4563e7124a
Vicky-codes17 <rameshvignesh0706@gmail.com>
2026-02-03 08:05:12 +0530

    refactor: add game module with config, states, and utils

---
+++ game/__init__.py
@@ -0,0 +1,8 @@
+"""
+Game core module
+SpeedyHighway v1.2.0
+"""
+
+from game.config import *
+from game.states import GameStates
+from game.utils import get_resource_path
---
+++ game/utils.py
@@ -0,0 +1,16 @@
+"""
+Utility functions
+SpeedyHighway v1.2.0
+"""
+
+import os
+import sys
+
+
+def get_resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
