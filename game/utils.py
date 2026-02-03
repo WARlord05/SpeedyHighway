4e9ee6e5b90594f2e95119e4a47d7534580c0689
Vignesh R <153487240+Vicky-codes17@users.noreply.github.com>
2026-02-03 08:09:11 +0530

    Clean up get_resource_path function
    Remove commented code about PyInstaller temp folder.

--- game/utils.py
+++ game/utils.py
@@ -9,7 +9,6 @@ import sys
 
 def get_resource_path(relative_path):
     try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except AttributeError:
         base_path = os.path.abspath(".")
