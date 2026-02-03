7be0cbe965ef1829cf9127e20c1fa7af10af762f
Vicky-codes17 <rameshvignesh0706@gmail.com>
2026-02-03 20:44:20 +0530

    Mod: Test for the main.py

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ v1.2.0
 
 """
 
-import os
-import warnings
-
-# Suppress pygame welcome message and AVX2 warning
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-warnings.filterwarnings('ignore', category=RuntimeWarning)
-
 from game.core import CarRacing
 
 def main():
