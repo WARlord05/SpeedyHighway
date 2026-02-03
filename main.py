37262d6881622299d4f359bc8c58daac86281a54
Vicky-codes17 <rameshvignesh0706@gmail.com>
2026-02-03 21:03:37 +0530

    Added: Removal of pygame message while running

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ v1.2.0
 
 """
 
+import os
+import warnings
+
+# Remove pygame support message
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+warnings.filterwarnings('ignore')
+
 from game.core import CarRacing
 
 def main():
